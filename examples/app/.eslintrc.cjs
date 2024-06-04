86202963e888ab0e2847cff0c71ac493741ded2a
Beatrice Guerra <beatrice.guerra@zextras.com>
2024-06-04 15:54:04 +0200

    docs: add example app and improve readme by adding basic usage instructions

---
+++ examples/app/.eslintrc.cjs
@@ -0,0 +1,24 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+}
---
+++ examples/app/README.md
@@ -0,0 +1,11 @@
+<!--
+SPDX-FileCopyrightText: 2024 Zextras <https://www.zextras.com>
+
+SPDX-License-Identifier: AGPL-3.0-only
+-->
+
+# @zextras/carbonio-ui-preview example app
+
+This app provide a minimal example of use of the carbonio-ui-preview package.
+
+Run `npm run dev` from within this folder to launch the dev environment and see it live.
