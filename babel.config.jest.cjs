29e1ef782e5ac4da42398a4845f1f20a7ec98ab7
Beatrice Guerra <beatrice.guerra@zextras.com>
2024-06-10 15:14:25 +0200

    build: remove redundant and incomplete exports field
    Refs: PREV-119

--- babel.config.jest.cjs
+++ babel.config.jest.cjs
@@ -4,10 +4,14 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 module.exports = {
-	env: {
-		test: {
-			plugins: ["@babel/plugin-transform-modules-commonjs"]
-		}
-	},
-	presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript']
+    presets: [
+		[
+			'@babel/preset-env',
+			{
+				modules: 'commonjs'
+			}
+		],
+		'@babel/preset-react',
+		'@babel/preset-typescript'
+	]
 };
